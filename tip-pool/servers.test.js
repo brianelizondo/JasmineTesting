d23a2f707f01416565dbaea6eba44d913c7c2da2
Brian Elizondo <brian_elizondo@msn.com>
2022-02-27 18:50:16 -0500

    tip-tool step one exercises completed

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,8 +11,20 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it('should add a row element and pass to appendTd on updateServerTable()', function(){
+    submitServerInfo();
+
+    const serversTdList = document.querySelectorAll('#serverTable tbody tr td');
+    expect(serversTdList.length).toEqual(2);
+    expect(serversTdList[0].innerText).toEqual('Alice');
+    expect(serversTdList[1].innerText).toEqual('$0.00');
+    // expect(serversTdList[2].innerText).toEqual('X');
+  });
+
   afterEach(function() {
     // teardown logic
-    serverNameInput.value = '';
+    serverId = 0;
+    allServers = {};
+    //serverTbody.innerHTML = '';
   });
 });
